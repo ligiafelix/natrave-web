471c46a9e30eb1c0cfe71e16ac053d89a7e032ae
Ligia Felix <ligiafelix5@gmail.com>
2022-10-25 17:49:27 -0300

    adicionando bandeiras

---
+++ README.md
@@ -0,0 +1,2 @@
+Desafio da codar.me para a criação de uma aplicação de palpites de jogos (Copa do Mundo 2022)
+web e mobile full-stack.
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,25 +2,45 @@ import { Icon } from '~/components'
 export const Dashboard = () => (
     <div className="">
 
-        <header className='bg-red-500 text-white'>
-            <div className='container max-w-3xl p-4 flex justify-between'>
+        <header className='bg-red-500 text-white p-2'>
+            <div className='container max-w-3xl flex justify-between p-4'>
                 <img src="/imgs/logo-red.svg" className="w-28 md:w-40" />
                 <a href="/profile">
-                    <Icon name="profile" className="w-10"/>
+                    <Icon name="profile" className="w-10" />
                 </a>
             </div>
         </header>
 
         <main>
             <section id='header' className=" bg-red-500 text-white p-4">
-                <div className='container max-w-3xl  space-y-2'>
+                <div className='container max-w-3xl  space-y-2 p-4'>
                     <span>Olá Lígia</span>
                     <h3 className='text-2xl font-blod'>Qual é o seu palpite?</h3>
                 </div>
             </section>
 
-            
-            <section id='content'>
+
+            <section id='content' className='container max-w-3xl p-4'>
+
+                <div className='rounded-xl border border-gray-300 p-4 text-center'>
+                    <span className='text-sm md:text-base text-gray-700 font-bold'>7:00</span>
+
+                    <div className='flex space-x-4 justify-center items-center'>
+
+                        <span className='uppercase'>sui</span>
+                        <img src="/imgs/flags/sui.png" />
+
+                        <input type="number" className='bg-red-300/[0.2] w-[55px] h-[55px] text-red-700 text-xl text-center' />
+                        <span className='text-red-500 font-bold'>X</span>
+                        <input type="number" className='bg-red-300/[0.2] w-[55px] h-[55px] text-red-700 text-xl text-center' />
+
+                        <img src="/imgs/flags/cam.png" />
+                        <span className='uppercase'>cam</span>
+
+
+
+                    </div>
+                </div>
 
             </section>
         </main>
